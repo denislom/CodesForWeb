28fc824b152f115da4ca22565187cc508d8126d0
Denis Lom <lom.denis@seznam.cz>
2023-03-03 02:18:56 +0100

    Dereferencing

--- main.c
+++ main.c
@@ -5,12 +5,19 @@ int main(void)
 	int	number;
 	int	*ptr;
 	
-	printf("Address of ptr: %p, Value of number %i\n", ptr, number);
-	number = 100;
-	printf("Address of ptr: %p, Value of number %i\n", ptr, number);
+	number = 5;
 	ptr = &number;
-	printf("Address of ptr: %p, Value of number %i\n", ptr, number);
-	printf("Address of ptr: %i, Value of number %i\n", ptr, number);
+	printf("%p\n", ptr);
+	number = 4;
+	printf("%i\n", *ptr);
+	printf("%i\n", *ptr);
+	printf("%i\n", number);
 
+	// printf("Address of ptr: %p, Value of number %i\n", ptr, number);
+	// number = 100;
+	// printf("Address of ptr: %p, Value of number %i\n", ptr, number);
+	// ptr = &number;
+	// printf("Address of ptr: %p, Value of number %i\n", ptr, number);
+	// printf("Address of ptr: %i, Value of number %i\n", ptr, number);
 	return (0);
 }
