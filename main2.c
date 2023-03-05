62311370916e9f38056c306d3162aebf9ca18679
Denis Lom <lom.denis@seznam.cz>
2023-03-05 23:28:09 +0100

    Dereferencing main2.c

---
+++ main2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int	number;
+	int	*ptr;
+
+	number = 100;
+	ptr = &number;
+	printf("Address of ptr: %p, Value of number %i\n", ptr, number);
+	printf("Value pointed to by a pointer variable ptr: %d, Value of number %i\n", *ptr, number);
+	return (0);
+}
