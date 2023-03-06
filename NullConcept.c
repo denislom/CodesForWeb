b8bfda2ad47ae6ae74fa6429ce84a522341c1102
Denis Lom <lom.denis@seznam.cz>
2023-03-07 00:05:36 +0100

    NullConcept

--- NullConcept.c
+++ NullConcept.c
@@ -3,8 +3,27 @@
 int main(void)
 {
 	int	*ptr;
+	int number;
 
-	ptr = NULL;
+	number = 1;
+	ptr = 0;
 	printf("Address of ptr: %p\n", ptr);
+	ptr = &number;
+	// ptr = NULL;
+	printf("Address of ptr: %p\n", ptr);
+
+	// if (ptr)
+	// {
+	// 	// ptr is not NULL
+	// }
+	// else
+	// {
+	// 	// ptr is NULL
+	// }
+
+	// if (ptr == NULL)
+	// 	// ptr is NULL
+	// if (ptr != NULL)
+	// 	// ptr is not NULL
 	return (0);
 }
--- main2.c
+++ main2.c
@@ -5,8 +5,10 @@ int main(void)
 	int	number;
 	int	*ptr;
 
+	ptr = 0; // Zero refers to the null pointer
 	number = 100;
-	ptr = &number;
+	ptr = &number; 
+	*ptr = 0; // Zero refers to the integer zero.
 	printf("Address of ptr: %p, Value of number %i\n", ptr, number);
 	printf("Value pointed to by a pointer variable ptr: %d, Value of number %i\n", *ptr, number);
 	*ptr = 5;
