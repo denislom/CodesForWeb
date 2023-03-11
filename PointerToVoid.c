ce0b7875f02346e41218ff96feebe006a777546c
Denis Lom <lom.denis@seznam.cz>
2023-03-11 22:51:28 +0100

    PointerToVoid.c modified

--- PointerToVoid.c
+++ PointerToVoid.c
@@ -7,8 +7,11 @@ int main(void)
 	void *ptrvoid;
 
 	printf("Address of ptr: %p\nAdress of ptrvoid %p\n", ptr, ptrvoid);
+	printf("\nnumber = 10;\nptrvoid = &number;\n\n");
 	number = 10;
 	ptrvoid = &number;
+	printf("Adress of ptrvoid %p\n", ptrvoid);
+	printf("\nptr = (int *)ptrvoid;\n\n");
 	ptr = (int *)ptrvoid;
 	printf("Address of ptr: %p\nAdress of ptrvoid %p\nValue pointed to by a pointer variable ptr: %d\n", ptr, ptrvoid, *ptr);
 
