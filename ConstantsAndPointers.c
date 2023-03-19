33f1921fe93d61c671a87aad8834a5d782e72732
Denis Lom <lom.denis@seznam.cz>
2023-03-19 19:47:13 +0100

    CoinstantsAndPointers.c changed with more examples

--- ConstantsAndPointers.c
+++ ConstantsAndPointers.c
@@ -7,14 +7,20 @@ int	main(void)
 	int			*ptr;
 	const int	*cptr;
 	int *const	constptr = &num;
+	int			num2;
 
 	num = 10;
+	num2 = num;
 	ptr = &num;
 	cptr = &limit;
+	num = 9;
 	printf("Address of num: %p\n", &num);
 	printf("Address of limit: %p\n", &limit);
 	printf("Address of ptr: %p\n", &ptr);
 	printf("Address of cptr: %p\n", &cptr);
+	printf("Address of constptr: %p\n", &constptr);
+	printf("Value of constptr: %d\n", *constptr);
+	*constptr = num2;
 	printf("Address of constptr: %p\n", &constptr);
 	printf("Value of constptr: %d\n", *constptr);
 	return (0);
