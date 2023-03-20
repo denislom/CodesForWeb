c65b613d6198ff7a090915b0724867500391abbc
Denis Lom <lom.denis@seznam.cz>
2023-03-20 23:47:11 +0100

    Memory leaks examples

--- MemoryLeaks.c
+++ MemoryLeaks.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// example of losing the address
+// int	main(void)
+// {
+// 	int	*ptr;
+
+// 	ptr = (int *)malloc(sizeof(int));
+// 	*ptr = 5;
+// 	ptr = (int *)malloc(sizeof(int));
+// 	free(ptr);
+// 	free(ptr);
+// 	return (0);
+// }
 
 // example of losing the address
 int	main(void)
 {
-	int	*ptr;
+	char	*name;
 
-	ptr = (int *)malloc(sizeof(int));
-	*ptr = 5;
-	ptr = (int *)malloc(sizeof(int));
-	free(ptr);
-	free(ptr);
+	name = (char *)malloc(sizeof("Toto" + 1));
+	strcpy(name, "Toto");
+	while (*name != '\0')
+	{
+		printf("%c", *name);
+		name++;
+	}
 	return (0);
 }
