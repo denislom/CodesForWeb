8a7d93d7b75ad45917d65f05dd27cc26ce835555
Denis Lom <lom.denis@seznam.cz>
2023-04-11 00:56:34 +0200

    explanation of stack

---
+++ stack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float	average(int *arr, int size)
+{
+	int	sum;
+	int	i;
+
+	sum = 0;
+	i = 0;
+	printf("arr: %p\n", &arr);
+	printf("size: %p\n", &size);
+	printf("sum: %p\n", &sum);
+	while (i < size)
+	{
+		sum += arr[i];
+		i++;
+	}
+	return ((sum * 1.0f) / size);
+}
+
+int	main(void)
+{
+	int	arr[] = {1, 2, 3};
+	printf("average %f:\n",	average(arr, 3));
+	return (0);
+}
