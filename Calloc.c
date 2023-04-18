f832cdfe60dd0bec50862afa25da0be6833ae939
Denis Lom <lom.denis@seznam.cz>
2023-04-19 01:03:16 +0200

    git pull --no-rebase

--- Calloc.c
+++ Calloc.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
+#include <string.h>
 
 int	main(void)
 {
 	int	*ptr;
 
-	ptr = calloc(5, sizeof(int));
+	ptr = (int *)malloc(5 * sizeof(int));
+	memset(ptr, 0, 5 * sizeof(int));
+	// ptr = calloc(5, sizeof(int));
 	free(ptr);
 	return (0);
 }
