73d1b5e947c4e0ed34bbd2a4b41dcf11845e2d49
Denis Lom <lom.denis@seznam.cz>
2023-04-19 00:59:08 +0200

    Passing data using a pointer

---
+++ swap.c
@@ -0,0 +1,19 @@
+void	swap(int *num1, int *num2)
+{
+	int	tmp;
+
+	tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
+
+int	main(void)
+{
+	int	i1;
+	int	i2;
+
+	i1 = 1;
+	i2 = 2;
+	swap(&i1, &i2);
+	retrun (0);
+}
